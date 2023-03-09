f2ca1888904139d3666836cb0294417314c93bbd
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2023-03-10 01:09:05 +0530

    Refactored code to monorepo

---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import minimist from "minimist";
+import {
+  ask,
+  baseCommands,
+  displaySpinnerAsync,
+  handle,
+  loadDefaultConfig,
+  parseInput,
+  State,
+  title,
+} from ".";
+
+// Register command type prompt
+const inquirerCommandPrompt = require("inquirer-command-prompt");
+inquirer.registerPrompt("command", inquirerCommandPrompt);
+
+async function onStartup() {
+  try {
+    await displaySpinnerAsync("Loading config...", loadDefaultConfig);
+    await displaySpinnerAsync(
+      "Connecting client...",
+      async () => await State.connectClient()
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function start() {
+  await onStartup();
+  const inputs = process.argv.slice(2);
+  if (inputs.length === 0) {
+    await title();
+    while (true) {
+      let input = await ask();
+      const { _: cmd, ...flags } = minimist(parseInput(input.command));
+      await handle(cmd, flags, baseCommands);
+    }
+  } else {
+    const { _: cmd, ...flags } = minimist(parseInput(inputs.join(" ")));
+    await handle(cmd, flags, baseCommands);
+    process.exit();
+  }
+}
+
+start();
