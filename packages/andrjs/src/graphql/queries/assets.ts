b967913a5bf866ac6aae4f72f7a2393e3820b2be
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-03-24 01:26:22 +0530

    Fix tx search command and added asset search filter

--- packages/andrjs/src/graphql/queries/assets.ts
+++ packages/andrjs/src/graphql/queries/assets.ts
@@ -1,5 +1,6 @@
 import { IAndrOrderBy } from "@andromedaprotocol/gql/dist/__generated/node";
 import { querySdk } from "../client";
+import { IAdoType } from "@andromedaprotocol/gql";
 
 /**
  * Queries all assets owned by a wallet address
@@ -13,14 +14,16 @@ export async function queryAssets(
     limit: number,
     offset: number,
     orderBy?: IAndrOrderBy,
-    search?: string
+    search?: string,
+    adoType?: IAdoType
 ) {
     const resp = await querySdk.ASSETS({
         walletAddress,
         limit,
         offset,
         orderBy,
-        search
+        search,
+        adoType
     })
 
     return resp.accounts.assets;
