ed360b0e02a65e7b45a652383dca2cb777a190a7
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-03-30 18:15:43 +0530

    Update gql url

--- packages/gql/codegen.master.ts
+++ packages/gql/codegen.master.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:8085/graphql',
+  schema: 'https://gql.testnet.andromedaprotocol.io/graphql',
   documents: 'schema/**/*.graphql',
   ignoreNoDocuments: true,
   generates: {
